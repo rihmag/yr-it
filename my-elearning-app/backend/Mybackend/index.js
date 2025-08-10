57c111cfe0a2f965dfa558c9ce0b437127a881fe
AnantG <148623507+rihmag@users.noreply.github.com>
2025-08-10 09:27:33 +0530

    let see

--- my-elearning-app/backend/Mybackend/index.js
+++ my-elearning-app/backend/Mybackend/index.js
@@ -16,7 +16,10 @@ db();
 
 // Middleware
 
-app.use(cors());
+app.use(cors({
+     origin: ["http://localhost:3000", "https://tamdekenaaltolda.vercel.app/login"],
+  credentials: true}
+));
 app.use(express.json())
 app.use('/api', routes);
 app.use('/api/auth', authRoutes);
